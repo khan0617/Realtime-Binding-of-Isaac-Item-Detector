1691fb23ff73a1e3409b5d695b6d987bfca0ed49
Hamza Khan <khan0617@umn.edu>
2024-07-28 16:42:41 -0400

    Fix alpha channel numpy<-->PIL bug and verify augmentations work. Add main().

--- src/data_augmentation/augmentation.py
+++ src/data_augmentation/augmentation.py
@@ -8,8 +8,8 @@ class Augmentation(StrEnum):
 
     ROTATE = "rotate"
     NOISE = "noise"
-    FLIP = "flip"  # vertical (top to bottom) flip
-    MIRROR = "mirror"  # horizontal (left to right) flip
+    VERTICAL_FLIP = "flip"  # vertical (top to bottom) flip
+    HORIZONTAL_MIRROR = "mirror"  # horizontal (left to right) flip
     BRIGHTNESS = "brightness"
     CONTRAST = "contrast"
     SCALE = "scale"
@@ -23,4 +23,4 @@ class Augmentation(StrEnum):
     def operations_to_not_repeat(cls) -> list[Augmentation]:
         """Returns a list of augmentations that do not make sense to repeat (those that are not randomized),
         such as FLIP or MIRROR."""
-        return [cls.FLIP, cls.MIRROR, cls.SMOOTH]
+        return [cls.VERTICAL_FLIP, cls.HORIZONTAL_MIRROR, cls.SMOOTH]
--- src/data_augmentation/data_augmentor.py
+++ src/data_augmentation/data_augmentor.py
@@ -2,13 +2,12 @@ import logging
 import os
 import random
 import uuid
+from collections.abc import Iterable
 
-import cv2
 import numpy as np
 from PIL import Image, ImageEnhance, ImageFilter, ImageOps
 
-ImageFilter.SMOOTH
-
+from constants import DATA_DIR, UNMODIFIED_FILE_NAME
 from data_augmentation.augmentation import Augmentation
 from logging_config import configure_logging
 
@@ -23,7 +22,7 @@ class DataAugmentor:
     def augment_image(
         image_path: str,
         output_dir: str,
-        augmentations: list[tuple[Augmentation, ...] | Augmentation] | None = None,
+        augmentations: Iterable[Iterable[Augmentation] | Augmentation] | None = None,
         num_augmented: int = 15,
         seed: int | None = None,
     ) -> None:
@@ -36,7 +35,7 @@ class DataAugmentor:
         Args:
             image_path (str): The file path of the original image to be augmented.
             output_dir (str): The directory where the augmented images will be saved. The directory will be created if it doesn't exist.
-            augmentations (list[Union[Augmentation, Tuple[Augmentation]]], optional): A list of Augmentation or tuples of
+            augmentations (Iterable[Union[Augmentation, Iterable[Augmentation]]], optional): An iterable of Augmentation or Iterables of
                 Augmentations specifying which augmentations to apply. If None, a default set of all augmentations is applied.
                 Ex: If you pass in `[(Augmentation.ROTATE, Augmentation.FLIP), Augmentation.NOISE]`, for the first tuple,
                     all augmented images will first be rotated, THEN flipped. For the 2nd item, those images will only have NOISE applied.
@@ -53,28 +52,31 @@ class DataAugmentor:
             augmentations = list(Augmentation)  # apply all the augmentations by default
 
         os.makedirs(output_dir, exist_ok=True)
-        image_np = np.array(Image.open(image_path))
+
+        # need to do convert() otherwise numpy doesn't preserve the png transparent backround and everything turns black.
+        image_pil = Image.open(image_path).convert(mode="RGBA")
+        image_np = np.asarray(image_pil)
 
         for aug in augmentations:
-            if isinstance(aug, tuple):
+            if isinstance(aug, Iterable) and not isinstance(aug, str):
                 DataAugmentor._apply_and_save_combined_augmentations(image_np, output_dir, aug, num_augmented)
             else:
                 DataAugmentor._apply_and_save_single_augmentation(image_np, output_dir, aug, num_augmented)
 
     @staticmethod
     def _apply_and_save_combined_augmentations(
-        image: np.ndarray, output_dir: str, augmentations: tuple[Augmentation, ...], num_augmented: int
+        image: np.ndarray, output_dir: str, augmentations: Iterable[Augmentation], num_augmented: int
     ) -> None:
         """Applies combined augmentations to an image and saves the result.
 
-        This method applies a tuple of augmentations in sequence to the provided image. If all augmentations
-        in the tuple are operations that do not produce unique results when repeated, it applies the
+        This method applies augmentations in sequence to the provided image. If all augmentations
+        in the iterable are operations that do not produce unique results when repeated, it applies the
         augmentations only once. Otherwise, it applies the sequence multiple times as specified by `num_augmented`.
 
         Args:
             image (np.ndarray): The original image to be augmented.
             output_dir (str): The directory where the augmented images will be saved.
-            augmentations (tuple[Augmentation, ...]): A tuple of Augmentation enum values indicating the sequence of augmentations to apply.
+            augmentations (Iterable[Augmentation, ...]): A collection of Augmentation enum values indicating the sequence of augmentations to apply.
             num_augmented (int): The number of times to apply the sequence of augmentations. This value is overridden to 1 if all augmentations
                                 are in the non-repeating category.
         """
@@ -107,14 +109,14 @@ class DataAugmentor:
 
     @staticmethod
     def _apply_and_save_helper(
-        image: np.ndarray, output_dir: str, augmentations: tuple[Augmentation, ...], num_augmented: int
+        image: np.ndarray, output_dir: str, augmentations: Iterable[Augmentation], num_augmented: int
     ) -> None:
         """Applies specified augmentations to an image and saves the augmented images.
 
         Args:
             image (np.ndarray): The original image to be augmented.
             output_dir (str): The directory where the augmented images will be saved.
-            augmentations (tuple[Augmentation, ...]): A tuple of Augmentation enum values specifying the augmentations to apply.
+            augmentations (Iterable[Augmentation, ...]): A collection of Augmentation enum values specifying the augmentations to apply.
             num_augmented (int): The number of augmented versions of the image to generate.
         """
         for _ in range(num_augmented):
@@ -124,7 +126,7 @@ class DataAugmentor:
             DataAugmentor._save_image(augmented_image, output_dir, augmentations)
 
     @staticmethod
-    def _save_image(image: np.ndarray, output_dir: str, augmentations: tuple[Augmentation, ...]) -> None:
+    def _save_image(image: np.ndarray, output_dir: str, augmentations: Iterable[Augmentation]) -> None:
         """Saves the augmented image to the specified directory with a descriptive filename.
 
         Images are saved with the list of augmentations applied to them in order. For example, if an image is
@@ -135,12 +137,13 @@ class DataAugmentor:
         Args:
             image (np.ndarray): The augmented image as a NumPy array.
             output_dir (str): The directory where the image will be saved.
-            augmentations (Tuple[Augmentation, ...]): A tuple of Augmentation enum values specifying the augmentations applied.
+            augmentations (Iterable[Augmentation, ...]): A collection of Augmentation enum values specifying the augmentations applied.
         """
         image_pil = Image.fromarray(image)
         augmentations_applied = "_".join(aug.lower() for aug in augmentations)
         filename = f"{augmentations_applied}_{uuid.uuid4().hex[:4]}.png"  # apply a random
         filepath = os.path.join(output_dir, filename)
+        logger.info("_save_image: Saving img to %s, image size is: %s", filepath, image_pil.size)
         image_pil.save(filepath)
 
     @staticmethod
@@ -162,11 +165,11 @@ class DataAugmentor:
         elif augmentation is Augmentation.NOISE:
             return DataAugmentor._noise(image)
 
-        elif augmentation is Augmentation.FLIP:
-            return DataAugmentor._flip(image)
+        elif augmentation is Augmentation.VERTICAL_FLIP:
+            return DataAugmentor._vertical_flip(image)
 
-        elif augmentation is Augmentation.MIRROR:
-            return DataAugmentor._mirror(image)
+        elif augmentation is Augmentation.HORIZONTAL_MIRROR:
+            return DataAugmentor._horizontal_mirror(image)
 
         elif augmentation is Augmentation.BRIGHTNESS:
             return DataAugmentor._brightness(image)
@@ -196,32 +199,38 @@ class DataAugmentor:
             raise ValueError(f"Unimplemented Augmentation: {augmentation}")
 
     @staticmethod
-    def _rotate(image: np.ndarray) -> np.ndarray:
-        """Rotates the image by a random angle +-60 degrees"""
-        angle = np.random.uniform(-60, 60)
+    def _rotate(image: np.ndarray) -> np.ndarray:  # tested! works :)
+        """Rotates the image by a random angle in the range +-170 degrees"""
+        angle = np.random.uniform(-170, 170)
         image_pil = Image.fromarray(image)
         rotated_image = image_pil.rotate(angle, resample=Image.Resampling.BICUBIC)
-        return np.array(rotated_image)
+        return np.asarray(rotated_image)
 
     @staticmethod
-    def _noise(image: np.ndarray) -> np.ndarray:
+    def _noise(image: np.ndarray) -> np.ndarray:  # tested, thing it works? but it does distort the
         """Adds random Gaussian noise to the image."""
         noise_level = np.random.uniform(0.01, 0.05)
-        noise = np.random.normal(0, 255 * noise_level, image.shape)  # gaussian centered at 0
-        noisy_image = np.clip(image + noise, 0, 255).astype(np.uint8)
-        return noisy_image
+        if image.shape[-1] == 4:  # RGBA image, let's not corrupt the alpha (transparency) channel.
+            rgb, alpha = image[..., :3], image[..., 3:]
+            noise = np.random.normal(0, 255 * noise_level, rgb.shape)  # apply noise only to RGB
+            noisy_rgb = np.clip(rgb + noise, 0, 255).astype(np.uint8)
+            return np.concatenate((noisy_rgb, alpha), axis=-1)
+        else:  # RGB image
+            noise = np.random.normal(0, 255 * noise_level, image.shape)
+            noisy_image = np.clip(image + noise, 0, 255).astype(np.uint8)
+            return noisy_image
 
     @staticmethod
-    def _flip(image: np.ndarray) -> np.ndarray:
+    def _vertical_flip(image: np.ndarray) -> np.ndarray:
         """Flip the imnage vertically (top to bottom flip)"""
         flipped_pil_img = ImageOps.flip(Image.fromarray(image))
-        return np.array(flipped_pil_img)
+        return np.asarray(flipped_pil_img)
 
     @staticmethod
-    def _mirror(image: np.ndarray) -> np.ndarray:
+    def _horizontal_mirror(image: np.ndarray) -> np.ndarray:
         "Mirror the image horizontally (left to right flip)"
         mirrored_pil_img = ImageOps.mirror(Image.fromarray(image))
-        return np.array(mirrored_pil_img)
+        return np.asarray(mirrored_pil_img)
 
     @staticmethod
     def _brightness(image: np.ndarray) -> np.ndarray:
@@ -229,7 +238,7 @@ class DataAugmentor:
         factor = np.random.uniform(0.5, 1.5)
         image_pil = Image.fromarray(image)
         bright_image = ImageEnhance.Brightness(image_pil).enhance(factor)
-        return np.array(bright_image)
+        return np.asarray(bright_image)
 
     @staticmethod
     def _contrast(image: np.ndarray) -> np.ndarray:
@@ -237,7 +246,7 @@ class DataAugmentor:
         factor = np.random.uniform(0.5, 1.5)
         image_pil = Image.fromarray(image)
         contrast_image = ImageEnhance.Contrast(image_pil).enhance(factor)
-        return np.array(contrast_image)
+        return np.asarray(contrast_image)
 
     @staticmethod
     def _scale(image: np.ndarray) -> np.ndarray:
@@ -246,7 +255,7 @@ class DataAugmentor:
         image_pil = Image.fromarray(image)
         scaled_image = ImageOps.scale(image_pil, scale_factor)
         scaled_image_back_to_original_size = ImageOps.fit(scaled_image, image_pil.size)
-        return np.array(scaled_image_back_to_original_size)
+        return np.asarray(scaled_image_back_to_original_size)
 
     @staticmethod
     def _translate(image: np.ndarray) -> np.ndarray:
@@ -269,7 +278,7 @@ class DataAugmentor:
         )  # https://en.wikipedia.org/wiki/Transformation_matrix#Affine_transformations
         image_pil = Image.fromarray(image)
         translated_image = image_pil.transform(image_pil.size, Image.Transform.AFFINE, translation_matrix)
-        return np.array(translated_image)
+        return np.asarray(translated_image)
 
     @staticmethod
     def _shear(image: np.ndarray) -> np.ndarray:
@@ -278,15 +287,15 @@ class DataAugmentor:
         image_pil = Image.fromarray(image)
         shear_matrix = (1, shear_factor, 0, 0, 1, 0)  # https://mathworld.wolfram.com/ShearMatrix.html
         sheared_image = image_pil.transform(image_pil.size, Image.Transform.AFFINE, shear_matrix)
-        return np.array(sheared_image)
+        return np.asarray(sheared_image)
 
     @staticmethod
     def _color_jitter(image: np.ndarray) -> np.ndarray:
-        """Randomly adjust color balance of the image, with a factor in range [0.5, 1.0]."""
-        color_factor = np.random.uniform(0.5, 1.0)
+        """Randomly adjust color balance of the image, with a factor in range [0.3, 1.0]."""
+        color_factor = np.random.uniform(0.3, 1.0)
         image_pil = Image.fromarray(image)
         jittered_image = ImageEnhance.Color(image_pil).enhance(color_factor)
-        return np.array(color_factor)
+        return np.asarray(jittered_image)
 
     @staticmethod
     def _sharpness(image: np.ndarray) -> np.ndarray:
@@ -294,7 +303,7 @@ class DataAugmentor:
         factor = np.random.uniform(0.5, 1.5)
         image_pil = Image.fromarray(image)
         sharpened_image = ImageEnhance.Sharpness(image_pil).enhance(factor)
-        return np.array(sharpened_image)
+        return np.asarray(sharpened_image)
 
     @staticmethod
     def _smooth(image: np.ndarray) -> np.ndarray:
@@ -308,4 +317,18 @@ class DataAugmentor:
         """
         image_pil = Image.fromarray(image)
         filtered_image = image_pil.filter(ImageFilter.SMOOTH)
-        return np.array(filtered_image)
+        return np.asarray(filtered_image)
+
+
+def main() -> None:
+    # example usage. We'll apply every augmentation to the "1up!" item.
+    item_name = "8_Inch_Nails"
+    image_path = os.path.join(DATA_DIR, item_name, UNMODIFIED_FILE_NAME)
+    output_dir = os.path.join("new_dir", item_name)
+
+    # produce 1 image for each augmentation.
+    DataAugmentor.augment_image(image_path, output_dir, num_augmented=1, seed=39)
+
+
+if __name__ == "__main__":
+    main()
